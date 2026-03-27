272d1ac21dc7820e4e9094be6cc7b40627c5769e
jchlee428-cyber <jchlee428@gmail.com>
2026-03-27 11:35:54 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/은혜의-묵상과-영적-멘토링-gracereflect---christian-meditation-assistant/', 
+      base: '/-/',
       server: {
         port: 3000,
         host: '0.0.0.0',
